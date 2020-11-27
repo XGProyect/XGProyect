208d5b84f141920e9c52fc739bcef0eecd660e1f
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-11-27 18:01:48 +0100

    - base settings

--- src/upload/Xgp/Lobby/Routes/web.php
+++ src/upload/Xgp/Lobby/Routes/web.php
@@ -9,8 +9,6 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::prefix('lobby')->group(function() {
-    Route::get('/', 'LobbyController@index');
-});
+Route::resource('/', 'HomeController');
