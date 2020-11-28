fd660c3f074fcefb369788ad5fe5eea674e3ee51
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-11-28 13:25:50 +0100

    - set global settings

--- src/upload/app/Providers/AppServiceProvider.php
+++ src/upload/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Option;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // view global variables
         view()->share('baseUrl', url('/'));
+
+        // set global settings
+        $this->setSettings();
+    }
+
+    /**
+     * Check if connection exists and load settings
+     *
+     * @return void
+     */
+    private function setSettings(): void
+    {
+        try {
+            if (DB::connection()->getPdo() && Schema::hasTable('options')) {
+                config([
+                    'settings' => Option::all(['option_name', 'option_value'])
+                        ->keyBy('name')
+                        ->transform(function ($setting) {
+                            return $setting->value; // return only the value
+                        })
+                        ->toArray(), // make it an array
+                ]);
+                var_dump(config('settings'));
+            }
+        } catch (\Exception $e) {
+            // do nothing
+        }
     }
 }
