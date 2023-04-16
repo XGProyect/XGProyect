5516a1b3b732bb023887c2c90ec72d5872b92fb1
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-04-16 23:49:48 +0200

    leave as an example

--- routes/legacy.php
+++ routes/legacy.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\LegacyController;
 use Illuminate\Support\Facades\Route;
 
+//Route::get('game.php', EmpireController::class)->where('page', 'empire');
 Route::any('{path}', LegacyController::class)->where('path', '.*');
