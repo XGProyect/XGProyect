eca64e3a9083a61274df56fc59cc122fdd0aacfb
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-04-17 19:45:56 +0200

    closest to laravel

--- legacy/app/Http/Controllers/Game/ForumController.php
+++ legacy/app/Http/Controllers/Game/ForumController.php
@@ -2,7 +2,7 @@
 
 namespace Xgp\App\Http\Controllers\Game;
 
-use Xgp\App\Core\BaseController;
+use Illuminate\Routing\Controller as BaseController;
 use Xgp\App\Libraries\Functions;
 use Xgp\App\Libraries\Users;
 
@@ -10,24 +10,12 @@ class ForumController extends BaseController
 {
     public const MODULE_ID = 14;
 
-    public function __construct()
-    {
-        parent::__construct();
-
-        Users::checkSession();
-    }
-
     public function __invoke(): void
     {
-        // Check module access
+        Users::checkSession();
+
         Functions::moduleMessage(Functions::isModuleAccesible(self::MODULE_ID));
 
-        // build the page
-        $this->buildPage();
-    }
-
-    private function buildPage(): void
-    {
         Functions::redirect(Functions::readConfig('forum_url'));
     }
 }
