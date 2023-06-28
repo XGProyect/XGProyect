bbb04add8a2da5f1b18806daae8acd23939e9cbf
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-06-28 20:15:59 +0200

    clean up legacy install

--- legacy/resources/views/install/save_message_view.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="alert {color}">
-                <strong>{status}</strong> {message}
-            </div>
-        </div>
-    </div>
-</div>
