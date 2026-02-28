10928a90f401307cb55b0a6a6b17b2bf42509dcd
Lucas Kovács <lucas.kovacs@gmail.com>
2026-02-28 09:18:37 +0100

    improve the view

--- resources/views/admin/update.blade.php
+++ resources/views/admin/update.blade.php
@@ -2,54 +2,38 @@
 
 @section('content')
 <div class="container-fluid">
-    <x-alert :dismissible=false />
-    @if ($continue)
+    <x-alert/>
+
     <form name="update_form" method="post" action="">
-        <input type="hidden" name="send" value="send">
-        <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">{{ __('admin/update.up_title') }}</h1>
-            <button type="submit" class="btn btn-primary btn-icon-split">
-                <span class="icon text-white-50">
-                    <i class="fas fa-sync-alt"></i>
-                </span>
+            @if ($continue)
+            <button type="submit" class="btn btn-primary btn-icon-split mt-3 mt-sm-0">
+                <span class="icon text-white-50"><i class="fas fa-sync-alt"></i></span>
                 <span class="text">{{ __('admin/update.up_go') }}</span>
             </button>
+            @endif
         </div>
         <p class="mb-4">{!! $up_sub_title !!}</p>
 
+        @if ($continue)
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-12">
                 <div class="card shadow mb-4">
-                    <!-- Card Header - Accordion -->
-                    <a href="#collapseGeneral" class="d-block card-header py-3" data-toggle="collapse" role="button"
-                        aria-expanded="true" aria-controls="collapseGeneral">
+                    <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">{{ __('admin/update.up_test_mode') }}</h6>
-                    </a>
-                    <!-- Card Content - Collapse -->
-                    <div class="collapse show" id="collapseGeneral" style="">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-borderless" width="100%" cellspacing="0">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <span>
-                                                    <input class="form-check-input" type="checkbox" name="demo_mode" checked>
-                                                    <label>{{ __('admin/update.up_test_mode') }}</label>
-                                                    <p><em>{!! __('admin/update.up_test_mode_notice') !!}</em></p>
-                                                </span>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" name="demo_mode" id="demo_mode" checked>
+                            <label class="custom-control-label" for="demo_mode">{{ __('admin/update.up_test_mode') }}</label>
                         </div>
+                        <p class="mt-2 mb-0 text-gray-600"><em>{!! __('admin/update.up_test_mode_notice') !!}</em></p>
                     </div>
                 </div>
             </div>
         </div>
+        @endif
     </form>
-    @endif
 </div>
 @endsection
