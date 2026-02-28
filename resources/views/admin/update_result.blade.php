81cb4654899b45ce284ff00878bc79f2a87779ef
Lucas Kovács <lucas.kovacs@gmail.com>
2026-02-28 09:22:28 +0100

    improve the view

--- resources/views/admin/update_result.blade.php
+++ resources/views/admin/update_result.blade.php
@@ -3,31 +3,24 @@
 @section('content')
 <div class="container-fluid">
     <x-alert/>
-    <!-- Page Heading -->
+
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">{{ __('admin/update.up_title') }}</h1>
-        <button type="submit" class="btn btn-primary btn-icon-split" onclick="window.location.href='/admin/update'; return false;">
-            <span class="icon text-white-50">
-                <i class="fas fa-chevron-left"></i>
-            </span>
+        <a href="/admin/update" class="btn btn-primary btn-icon-split mt-3 mt-sm-0">
+            <span class="icon text-white-50"><i class="fas fa-chevron-left"></i></span>
             <span class="text">{{ __('admin/update.up_back') }}</span>
-        </button>
+        </a>
     </div>
     <p class="mb-4">{!! $up_sub_title !!}</p>
 
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-12">
             <div class="card shadow mb-4">
-                <!-- Card Header - Accordion -->
-                <a href="#collapseGeneral" class="d-block card-header py-3" data-toggle="collapse" role="button"
-                    aria-expanded="true" aria-controls="collapseGeneral">
+                <div class="card-header py-3">
                     <h6 class="m-0 font-weight-bold text-primary">{{ __('admin/update.up_test_mode') }}</h6>
-                </a>
-                <!-- Card Content - Collapse -->
-                <div class="collapse show" id="collapseGeneral" style="">
-                    <div class="card-body">
-                        <pre>{{ $result }}</pre>
-                    </div>
+                </div>
+                <div class="card-body">
+                    <pre>{{ $result }}</pre>
                 </div>
             </div>
         </div>
