ebe4366c08a8db028343e9fb3ed86e2863d95eb3
Lucas Kovács <lucas.kovacs@gmail.com>
2026-02-28 15:21:13 +0100

    center encrypt

--- resources/views/admin/encrypter.blade.php
+++ resources/views/admin/encrypter.blade.php
@@ -10,54 +10,35 @@
         </div>
         <p class="mb-4">{{ __('admin/encrypter.et_sub_title') }}</p>
 
-        <div class="row">
-            <div class="col-lg-6">
+        <div class="row justify-content-center">
+            <div class="col-xl-8 col-lg-10">
                 <div class="card shadow mb-4">
-                    <!-- Card Header - Accordion -->
-                    <a href="#collapseGeneral" class="d-block card-header py-3" data-toggle="collapse" role="button"
-                        aria-expanded="true" aria-controls="collapseGeneral">
+                    <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">{{ __('admin/encrypter.et_general') }}</h6>
-                    </a>
-                    <!-- Card Content - Collapse -->
-                    <div class="collapse show" id="collapseGeneral" style="">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-borderless" width="100%" cellspacing="0">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <input class="form-control" type="text" name="unencrypted"
-                                                    value="{{ $unencrypted }}" placeholder="{{ __('admin/encrypter.et_pass') }}">
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="input-group mb-3">
-                                                    <div class="input-group-prepend">
-                                                        <span class="input-group-text" onclick="navigator.clipboard.writeText($('#encrypted').val());" style="cursor: pointer">
-                                                            <i class="fas fa-copy"></i>
-                                                        </span>
-                                                    </div>
-                                                    <input type="text" class="form-control" id="encrypted" name="encrypted" value="{{ $encrypted }}" disabled>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary btn-icon-split">
-                                        <span class="icon text-white-50">
-                                            <i class="fas fa-save"></i>
-                                        </span>
-                                        <span class="text">{{ __('admin/encrypter.et_encript') }}</span>
-                                    </button>
-                                </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <input class="form-control" type="text" name="unencrypted"
+                                value="{{ $unencrypted }}" placeholder="{{ __('admin/encrypter.et_pass') }}">
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" onclick="navigator.clipboard.writeText($('#encrypted').val());" style="cursor: pointer">
+                                    <i class="fas fa-copy"></i>
+                                </span>
                             </div>
+                            <input type="text" class="form-control" id="encrypted" name="encrypted" value="{{ $encrypted }}" disabled>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary btn-icon-split">
+                                <span class="icon text-white-50">
+                                    <i class="fas fa-save"></i>
+                                </span>
+                                <span class="text">{{ __('admin/encrypter.et_encript') }}</span>
+                            </button>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
             </div>
         </div>
     </form>
