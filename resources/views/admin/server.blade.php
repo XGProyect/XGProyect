25cefc8b372d447c69592b492cca3070a480a1f8
Lucas Kovács <lucas.kovacs@gmail.com>
2026-03-02 15:19:06 +0100

    re organize the view - makes more sense

--- resources/views/admin/server.blade.php
+++ resources/views/admin/server.blade.php
@@ -17,55 +17,26 @@
             </x-slot>
         </x-admin.page-header>
 
-        {{-- Row 1: Identity | Speed & Economy --}}
+        {{-- Row 1: Server Access | Speed & Economy --}}
         <div class="row">
             <div class="col-lg-6">
-                <x-admin.card-collapsible id="collapseIdentity" :title="__('admin/server.se_section_identity')" icon="fas fa-globe">
+                <x-admin.card-collapsible id="collapseAccess" :title="__('admin/server.se_section_access')" icon="fas fa-power-off">
                     <x-admin.settings-table>
                         <tr>
+                            <td>{{ __('admin/server.se_server_op_close') }}</td>
                             <td>
-                                {{ __('admin/server.se_name') }}
-                                <i class="fas fa-question-circle text-gray-400" data-toggle="popover"
-                                    data-trigger="hover" data-content="{{ __('admin/server.se_server_name') }}"
-                                    data-html="true"></i>
-                            </td>
-                            <td>
-                                <input class="form-control" type="text" name="game_name"
-                                    value="{{ $game_name }}" maxlength="60">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="game_enable"
+                                        name="game_enable" {{ $game_enable ? 'checked' : '' }}>
+                                    <label class="custom-control-label" for="game_enable"></label>
+                                </div>
                             </td>
                         </tr>
                         <tr>
+                            <td>{{ __('admin/server.se_server_status_message') }}</td>
                             <td>
-                                {{ __('admin/server.se_logo') }}
-                                <i class="fas fa-question-circle text-gray-400" data-toggle="popover"
-                                    data-trigger="hover" data-content="{{ __('admin/server.se_server_logo') }}"
-                                    data-html="true"></i>
-                            </td>
-                            <td>
-                                <input class="form-control" type="text" name="game_logo"
-                                    value="{{ $game_logo }}" placeholder="https://...">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_lang') }}</td>
-                            <td>
-                                <select class="form-control" name="language">
-                                    {!! $language_settings !!}
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_admin_email') }}</td>
-                            <td>
-                                <input class="form-control" type="email" name="admin_email"
-                                    maxlength="254" value="{{ $admin_email }}">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_forum_link') }}</td>
-                            <td>
-                                <input class="form-control" type="url" name="forum_url"
-                                    maxlength="254" value="{{ $forum_url }}" placeholder="https://...">
+                                <textarea class="form-control" name="close_reason"
+                                    rows="4">{{ $close_reason }}</textarea>
                             </td>
                         </tr>
                     </x-admin.settings-table>
@@ -116,71 +87,7 @@
             </div>
         </div>
 
-        {{-- Row 2: Server Access | Date & Time --}}
-        <div class="row">
-            <div class="col-lg-6">
-                <x-admin.card-collapsible id="collapseAccess" :title="__('admin/server.se_section_access')" icon="fas fa-power-off">
-                    <x-admin.settings-table>
-                        <tr>
-                            <td>{{ __('admin/server.se_server_op_close') }}</td>
-                            <td>
-                                <div class="custom-control custom-switch">
-                                    <input type="checkbox" class="custom-control-input" id="game_enable"
-                                        name="game_enable" {{ $game_enable ? 'checked' : '' }}>
-                                    <label class="custom-control-label" for="game_enable"></label>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_server_status_message') }}</td>
-                            <td>
-                                <textarea class="form-control" name="close_reason"
-                                    rows="4">{{ $close_reason }}</textarea>
-                            </td>
-                        </tr>
-                    </x-admin.settings-table>
-                </x-admin.card-collapsible>
-            </div>
-
-            <div class="col-lg-6">
-                <x-admin.card-collapsible id="collapseDateTime" :title="__('admin/server.se_section_datetime')" icon="fas fa-clock">
-                    <x-admin.settings-table>
-                        <tr>
-                            <td>{{ __('admin/server.se_date_time_zone') }}</td>
-                            <td>
-                                <select class="form-control" name="date_time_zone">
-                                    @foreach ($timezone_options as $group)
-                                        <optgroup label="{{ $group['group'] }}">
-                                            @foreach ($group['zones'] as $zone)
-                                                <option value="{{ $zone['value'] }}" {{ $zone['selected'] ? 'selected' : '' }}>
-                                                    {{ $zone['label'] }}
-                                                </option>
-                                            @endforeach
-                                        </optgroup>
-                                    @endforeach
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_date_format') }}</td>
-                            <td>
-                                <input class="form-control" type="text" name="date_format"
-                                    value="{{ $date_format }}" placeholder="d/m/Y">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{ __('admin/server.se_date_format_extended') }}</td>
-                            <td>
-                                <input class="form-control" type="text" name="date_format_extended"
-                                    value="{{ $date_format_extended }}" placeholder="d/m/Y H:i:s">
-                            </td>
-                        </tr>
-                    </x-admin.settings-table>
-                </x-admin.card-collapsible>
-            </div>
-        </div>
-
-        {{-- Row 3: Combat Rules | Noob Protection --}}
+        {{-- Row 2: Combat Rules | Noob Protection --}}
         <div class="row">
             <div class="col-lg-6">
                 <x-admin.card-collapsible id="collapseCombat" :title="__('admin/server.se_section_combat')" icon="fas fa-crosshairs">
@@ -273,6 +180,99 @@
                             <td>
                                 <input class="form-control" type="number" name="noobprotectionmulti"
                                     value="{{ $noobprotectionmulti }}" min="0" max="99">
+                            </td>
+                        </tr>
+                    </x-admin.settings-table>
+                </x-admin.card-collapsible>
+            </div>
+        </div>
+
+        {{-- Row 3: Identity | Date & Time --}}
+        <div class="row">
+            <div class="col-lg-6">
+                <x-admin.card-collapsible id="collapseIdentity" :title="__('admin/server.se_section_identity')" icon="fas fa-globe">
+                    <x-admin.settings-table>
+                        <tr>
+                            <td>
+                                {{ __('admin/server.se_name') }}
+                                <i class="fas fa-question-circle text-gray-400" data-toggle="popover"
+                                    data-trigger="hover" data-content="{{ __('admin/server.se_server_name') }}"
+                                    data-html="true"></i>
+                            </td>
+                            <td>
+                                <input class="form-control" type="text" name="game_name"
+                                    value="{{ $game_name }}" maxlength="60">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ __('admin/server.se_logo') }}
+                                <i class="fas fa-question-circle text-gray-400" data-toggle="popover"
+                                    data-trigger="hover" data-content="{{ __('admin/server.se_server_logo') }}"
+                                    data-html="true"></i>
+                            </td>
+                            <td>
+                                <input class="form-control" type="text" name="game_logo"
+                                    value="{{ $game_logo }}" placeholder="https://...">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ __('admin/server.se_lang') }}</td>
+                            <td>
+                                <select class="form-control" name="language">
+                                    {!! $language_settings !!}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ __('admin/server.se_admin_email') }}</td>
+                            <td>
+                                <input class="form-control" type="email" name="admin_email"
+                                    maxlength="254" value="{{ $admin_email }}">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ __('admin/server.se_forum_link') }}</td>
+                            <td>
+                                <input class="form-control" type="url" name="forum_url"
+                                    maxlength="254" value="{{ $forum_url }}" placeholder="https://...">
+                            </td>
+                        </tr>
+                    </x-admin.settings-table>
+                </x-admin.card-collapsible>
+            </div>
+
+            <div class="col-lg-6">
+                <x-admin.card-collapsible id="collapseDateTime" :title="__('admin/server.se_section_datetime')" icon="fas fa-clock">
+                    <x-admin.settings-table>
+                        <tr>
+                            <td>{{ __('admin/server.se_date_time_zone') }}</td>
+                            <td>
+                                <select class="form-control" name="date_time_zone">
+                                    @foreach ($timezone_options as $group)
+                                        <optgroup label="{{ $group['group'] }}">
+                                            @foreach ($group['zones'] as $zone)
+                                                <option value="{{ $zone['value'] }}" {{ $zone['selected'] ? 'selected' : '' }}>
+                                                    {{ $zone['label'] }}
+                                                </option>
+                                            @endforeach
+                                        </optgroup>
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ __('admin/server.se_date_format') }}</td>
+                            <td>
+                                <input class="form-control" type="text" name="date_format"
+                                    value="{{ $date_format }}" placeholder="d/m/Y">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ __('admin/server.se_date_format_extended') }}</td>
+                            <td>
+                                <input class="form-control" type="text" name="date_format_extended"
+                                    value="{{ $date_format_extended }}" placeholder="d/m/Y H:i:s">
                             </td>
                         </tr>
                     </x-admin.settings-table>
