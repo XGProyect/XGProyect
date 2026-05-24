0f6795e299bce1a829f436c0a056ca84c60e1b2f
Lucas Kovács <lucas.kovacs@gmail.com>
2026-05-24 13:56:14 +0200

    fix - fleets with resources

--- resources/views/overview/fleets_popup.blade.php
+++ resources/views/overview/fleets_popup.blade.php
@@ -1,14 +1,14 @@
 <table style="width:200px; color:white;">
     <tr>
-        <td width="50%" align="left">{{ $metal }}</td>
+        <td width="50%" align="left">{{ __('game/global.metal') }}</td>
         <td width="50%" align="right">{{ $fleet_resource_metal }}</td>
     </tr>
     <tr>
-        <td width="50%" align="left">{{ $crystal }}</td>
+        <td width="50%" align="left">{{ __('game/global.crystal') }}</td>
         <td width="50%" align="right">{{ $fleet_resource_crystal }}</td>
     </tr>
     <tr>
-        <td width="50%" align="left">{{ $deuterium }}</td>
+        <td width="50%" align="left">{{ __('game/global.deuterium') }}</td>
         <td width="50%" align="right">{{ $fleet_resource_deuterium }}</td>
     </tr>
 </table>
